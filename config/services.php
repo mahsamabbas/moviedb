d699d985535b0b446afca53c04725ce5d2571d15
mahsamabbas <syedmahsam@gmail.com>
2021-06-02 20:12:33 +0500

    added keys for tmdb in config

--- config/services.php
+++ config/services.php
@@ -29,5 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'tmdb' => [
+        'key' => env('TMDB_API_KEY'),
+        'url' => env('TMDB_BASEURL')
+    ]
 
 ];
